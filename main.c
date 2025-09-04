bf5486bff097bf44986a0cbb216578a9f0ad6a85
mietempura <felcia1011@gmail.com>
2025-09-04 21:41:19 +0700

    init2

--- main.c
+++ main.c
@@ -4,17 +4,31 @@
 int main()
 {
     char nama[100];
+    int age;
 
-    printf("Insert your full name:");
+    printf("Insert your full name for verification:");
     scanf("%[^\n]", nama);
+    //"%[^\n]" scan sampai new line
 
     if (strcmp("Felcia Luhur", nama) == 0)
     {
-        printf("Your NIM is 2902658882 and your favorite language is C");
+        printf("Welcome!");
     }
     else
     {
         printf("Who are you?");
+    }
+
+    printf("\nfor further verification, how old are you?");
+    scanf("%d", &age);
+
+    if(age == 17)
+    {
+        printf("You are Felcia! Come in!");
+    }
+    else
+    {
+        printf("You're not her!");
     }
 
     return 0;
---
+++ print.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("Name : Felcia Luhur \nFavorite Programming Language : C");
+    return 0;
+}
